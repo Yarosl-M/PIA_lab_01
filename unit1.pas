83dc91b43267defcc7b871bacfe8438f1f3a38bf
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-09-13 21:27:57 +0300

    Initial commit

---
+++ unit1.pas
@@ -0,0 +1,137 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Grids,
+  RTTIGrids, TAGraph, Math;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BtnGenerate: TButton;
+    BtnExit: TButton;
+    BtnClear: TButton;
+    BtnChart: TButton;
+    BtnFindC: TButton;
+    LeCharterino: TChart;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    LabelC: TLabel;
+    Label5: TLabel;
+    ArrTable: TStringGrid;
+    procedure BtnClearClick(Sender: TObject);
+    procedure BtnExitClick(Sender: TObject);
+    procedure BtnFindCClick(Sender: TObject);
+    procedure BtnGenerateClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  MainArr: array[0..29] of Integer;
+  C: Integer;
+
+const
+  A : integer = 30;
+  B : integer = 80;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+function CellRow(i: integer): integer;
+begin
+  cellRow := i div 5;
+end;
+
+function CellCol(i: integer): integer;
+begin
+  cellCol := i mod 5;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  year: word;
+  myCaption: string;
+  captions: array[0..4] of String = (
+  'Bruh',
+  'Ronald Topala did not die for this',
+  'Really dingles my berries',
+  'M. A. is really proud',
+  'A true society moment'
+  );
+begin
+  Randomize();
+  year := currentyear();
+  myCaption := 'Pascal in ' + inttostr(year) + '?' + ' ' + captions[Random(5)];
+  caption := myCaption;
+end;
+
+
+
+procedure TForm1.BtnExitClick(Sender: TObject);
+begin
+  Close();
+end;
+
+procedure TForm1.BtnFindCClick(Sender: TObject);
+var
+  average, minDistance, thisMinDist: real;
+  minDistIndex, i: integer;
+begin
+  average := Mean(MainArr);
+  minDistance := abs(average - MainArr[0]);
+  minDistIndex := 0;
+  for i := 1 to 29 do
+  begin
+    thisMinDist := abs(average - MainArr[i]);
+    if thisMinDist < minDistance then
+    begin
+      minDistance := thisMinDist;
+      minDistIndex := i;
+    end;
+  end;
+  C := MainArr[minDistIndex];
+  LabelC.Caption := 'C = ' + inttostr(C);
+end;
+
+procedure TForm1.BtnClearClick(Sender: TObject);
+var
+  i: integer;
+begin
+  for i := 0 to 29 do
+  begin
+    ArrTable.Cells[CellCol(i), CellRow(i)] := '';
+  end;
+  BtnChart.Enabled := false;
+  BtnFindC.Enabled := false;
+  LabelC.Caption := 'C =';
+end;
+
+procedure TForm1.BtnGenerateClick(Sender: TObject);
+var
+  i : integer;
+begin
+  for i := 0 to 29 do
+  begin
+    MainArr[i] := Random(B - A + 1) + A;
+    ArrTable.Cells[CellCol(i), CellRow(i)] := inttostr(MainArr[i]);
+  end;
+  BtnChart.Enabled := true;
+  BtnFindC.Enabled := true;
+end;
+
+end.
+
